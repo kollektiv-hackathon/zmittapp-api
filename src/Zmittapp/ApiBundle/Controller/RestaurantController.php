1897e3d3a58ca1057b9f97ba81ebe1683246c63a
Marc Juchli <mail@marcjuch.li>
2014-11-01 21:41:54 +0100

    put restaurant working

--- src/Zmittapp/ApiBundle/Controller/RestaurantController.php
+++ src/Zmittapp/ApiBundle/Controller/RestaurantController.php
@@ -13,6 +13,7 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\HttpFoundation\Request;
 use Zmittapp\ApiBundle\Entity\MenuItem;
 use Zmittapp\ApiBundle\Entity\Restaurant;
@@ -127,9 +128,9 @@ class RestaurantController extends FOSRestController
     public function postRestaurantsAction(Request $request){
         try {
             $form = $this->createForm(new RestaurantType(), new Restaurant(), array('method' => 'POST'));
-            $newRestaurant = $this->get('zmittapp_api.form_handler.restaurant')->handle($form, $request);
+            $new = $this->get('zmittapp_api.form_handler.restaurant')->handle($form, $request);
             $routeOptions = array(
-                'id' => $newRestaurant->getId(),
+                'id' => $new->getId(),
                 '_format' => $request->get('_format')
             );
             return $this->routeRedirectView('restaurant_get', $routeOptions, Codes::HTTP_CREATED);
@@ -137,6 +138,50 @@ class RestaurantController extends FOSRestController
             return $exception->getForm();
         }
     }
+
+    /**
+     * Update an existing Restaurant
+     *
+     * @ApiDoc(
+     *  description="Update an existing Restaurant",
+     *  input = "Zmittapp\ApiBundle\Form\Type\RestaurantType",
+     *  statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the Restaurant not found"
+     *  }
+     * )
+     *
+     * @Route("/{id}", name="restaurant_put", defaults={"_format" = "json"})
+     * @Method("PUT")
+     * @Rest\View
+     */
+    public function putAction(Request $request, $id){
+        try {
+            $manager = $this->get('zmittapp_api.domain_manager.restaurant');
+            $formHandler = $this->get('zmittapp_api.form_handler.restaurant');
+
+            if (!($object = $manager->get($id))) {
+                $statusCode = Codes::HTTP_CREATED;
+                $form = $this->createForm(new RestaurantType(), new Restaurant(), array('method' => 'POST'));
+            } else {
+                $statusCode = Codes::HTTP_NO_CONTENT;
+                $form = $this->createForm(new RestaurantType(), $object, array('method' => 'PUT'));
+            }
+
+            $object = $formHandler->handle($form, $request);
+
+            $routeOptions = array(
+                'id' => $object->getId(),
+                '_format' => $request->get('_format')
+            );
+
+            return $this->routeRedirectView('restaurant_get', $routeOptions, $statusCode);
+
+        } catch (InvalidFormException $exception) {
+            return $exception->getForm();
+        }
+    }
+
 
     /**
      * List all menu items for a restaurant.
@@ -180,9 +225,9 @@ class RestaurantController extends FOSRestController
     public function postMenuItemAction(Request $request){
         try {
             $form = $this->createForm(new MenuItemType(), new MenuItem(), array('method' => 'POST'));
-            $newRestaurant = $this->get('zmittapp_api.form_handler.menuitem')->handle($form, $request);
+            $new = $this->get('zmittapp_api.form_handler.menuitem')->handle($form, $request);
             $routeOptions = array(
-                'id' => $newRestaurant->getId(),
+                'id' => $new->getRestaurant()->getId(),
                 '_format' => $request->get('_format')
             );
             return $this->routeRedirectView('restaurant_get', $routeOptions, Codes::HTTP_CREATED);
