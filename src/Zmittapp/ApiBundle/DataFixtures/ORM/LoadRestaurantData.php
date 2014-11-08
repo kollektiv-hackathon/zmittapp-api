5ed47dca31008b1ca3cbcec07076cfae51e15092
Marc Juchli <mail@marcjuch.li>
2014-11-08 19:11:43 +0100

    adding more restaurants to fixtures

--- src/Zmittapp/ApiBundle/DataFixtures/ORM/LoadRestaurantData.php
+++ src/Zmittapp/ApiBundle/DataFixtures/ORM/LoadRestaurantData.php
@@ -28,18 +28,42 @@ class LoadRestaurantData extends AbstractFixture implements OrderedFixtureInterf
         $restaurant->setName('Kronenhalle');
         $restaurant->setEmail('info@kornenhalle.ch');
         $restaurant->setPhone('+41 44 262 99 00');
-        $restaurant->setLat(47.367584);
-        $restaurant->setLon(8.545758);
+        $restaurant->setLat(47.3676249);
+        $restaurant->setLon(8.5458653);
 
         $restaurant2 = new Restaurant();
         $restaurant2->setName('Aubrey');
         $restaurant2->setEmail('info@kornenhalle.ch');
         $restaurant2->setPhone('+41 44 440 00 20');
-        $restaurant2->setLat(47.389125);
-        $restaurant2->setLon(8.517664);
+        $restaurant2->setLat(47.3891168);
+        $restaurant2->setLon(8.5177954);
+
+        $restaurant3 = new Restaurant();
+        $restaurant3->setName('les halles');
+        $restaurant3->setEmail('info@leshalles.ch');
+        $restaurant3->setPhone('+41 44 440 00 20');
+        $restaurant3->setLat(47.3882397);
+        $restaurant3->setLon(8.5186537);
+
+        $restaurant4 = new Restaurant();
+        $restaurant4->setName('Brasserie Lipp');
+        $restaurant4->setEmail('info@lipp.ch');
+        $restaurant4->setPhone('+41 44 440 00 20');
+        $restaurant4->setLat(47.3744784);
+        $restaurant4->setLon(8.5395551);
+
+        $restaurant5 = new Restaurant();
+        $restaurant5->setName('Bärengasse');
+        $restaurant5->setEmail('info@restaurant-baerengasse.ch');
+        $restaurant5->setPhone('+41 44 440 00 20');
+        $restaurant5->setLat(47.3707062);
+        $restaurant5->setLon(8.539184);
 
         $manager->persist($restaurant);
         $manager->persist($restaurant2);
+        $manager->persist($restaurant3);
+        $manager->persist($restaurant4);
+        $manager->persist($restaurant5);
 
         $menuItem1 = new MenuItem();
         $menuItem1->setDate('');
